7cf3d06ddef1683ffdcfa2551008e87390027ca3
Sneha <sssnehshah24@gmail.com>
2019-05-19 18:59:04 -0400

    a

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var app = express();
 
 
 
-app.use('/', express.static('public'));
+app.use(express.static('public'));
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT);
 
 app.listen(app.get('port'), function() {
     console.log("App running on port : ", app.get('port'));
