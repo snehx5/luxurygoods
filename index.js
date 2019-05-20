2fb8fc91dc4f3c03d0a6eff6d627b43b624ae8a9
Sneha <sssnehshah24@gmail.com>
2019-05-20 16:23:35 -0400

    added product page

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ var app = express();
 
 
 
-
 app.use(express.static('public'));
 
-app.set('port', (process.env.PORT|| 8888));
+app.set('port', (process.env.PORT|| 3000));
 
 app.listen(app.get('port'), function() {
     console.log("App running on port : ", app.get('port'));
